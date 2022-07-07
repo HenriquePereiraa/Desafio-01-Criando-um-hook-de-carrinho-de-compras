936a69babc58d176ea0094a0b3877dfb01077c52
Henrique Pereira <henrique.pereira@liferay.com>
2022-07-07 15:38:58 -0300

    em progresso

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,11 +8,8 @@ import { useCart } from "../../hooks/useCart";
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
-  const cartSize = cart?.reduce((acc, item) => {
-    acc++;
 
-    return acc;
-  }, 0);
+  const cartSize = cart.length
 
   return (
     <Container>
